8676f810bb8401c18a8b9b31342c02045857c32d
pilgrim <useandshed@gmail.com>
2020-01-19 18:56:37 +0300

    Revert "Revert "Work on #3""
    This reverts commit 00ad5d0e74df376b77f3db7d63489d79b33250de.

--- src/board.gd
+++ src/board.gd
@@ -95,7 +95,7 @@ func move(col : int, by_who: String, id: int, color: Color) -> bool:
 
 func undo_last_move() -> bool:
 	if len(moves) == 0:
-		return false
+		return true
 	
 	var row: int = moves.back()[0]
 	var col: int = moves.back()[1]
